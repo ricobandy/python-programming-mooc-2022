16e4f5f8395e20e82cd2dd20e2ca26000161769a
Charles Quaye <ricobandy@gmail.com>
2022-03-30 08:52:49 +0200

    part-01 updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-programming-mooc-2022
- 
+
+Solutions to Python Programming MOOC 2022 - https://programming-22.mooc.fi
---
+++ part-01/10_story.py
@@ -0,0 +1,7 @@
+# Write your solution here
+name = input("Please type in a name: ")
+year = input("Please type in a year: ")
+print()
+print(name + " is valiant knight, born in the year " + year + ". One morning " + name)
+print("woke up to an awful racket: a dragon was approaching the village. Only " + name)
+print("could save the village's residents.")
